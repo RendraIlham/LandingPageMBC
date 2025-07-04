fc747be5a39977695fd614ce842766ccd2e38d44
RendraIlham <rend124ilham@gmail.com>
2025-07-04 20:11:18 +0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.getElementById("tombol").addEventListener("click", function () {
-    document.getElementById("menu").classList.toggle("show");
+document.getElementById("tombol1").addEventListener("click", function () {
+    document.getElementById("menu1").classList.toggle("show");
+});
+
+document.getElementById("tombol2").addEventListener("click", function () {
+    document.getElementById("menu2").classList.toggle("show");
 });
